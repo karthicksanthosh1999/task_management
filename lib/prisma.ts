b58547ff464822fd17fc941d0535c2304d9f098e
karthicksanthosh <karthick.santhosh1999@gmail.com>
2025-11-27 21:54:37 +0530

    Fix prisma import path

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,10 @@
-import { PrismaClient } from "./generated/prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
+// import "dotenv/config";
+// import { PrismaPg } from "@prisma/adapter-pg";
+// import { PrismaClient } from "./generated/prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
-};
+// const connectionString = `${process.env.DATABASE_URL}`;
 
-const prisma =
-  globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+// const adapter = new PrismaPg({ connectionString });
+// const prisma = new PrismaClient({ adapter });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-export default prisma;
+// export { prisma };
